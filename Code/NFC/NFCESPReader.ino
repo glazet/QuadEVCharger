228f568b1234e2e3477e2b415486c9c638ab3540
Calvin Pereira <78508059+CalvinJDP@users.noreply.github.com>
2024-03-02 19:15:47 -0800

    Update NFCESPReader.ino
    Updated pin numbers. Fixed not compiling issue.

--- Code/NFC/NFCESPReader.ino
+++ Code/NFC/NFCESPReader.ino
@@ -5,48 +5,57 @@
 // Author: Smart Quad EV Charger Development Team
 
 
-#include <SPI.h>               // Include the SPI library for communication with peripherals
-#include <MFRC522.h>           // Include the MFRC522 library for interacting with the RFID module
+#include <SPI.h>
+#include <MFRC522.h>
 
-#define SS_PIN 10               // SDA
-#define RST_PIN 14              // RST
-#define SCK_PIN 367             // SCK
-#define MOSI_PIN 39             // MOSI
-#define MISO_PIN 13             // MISO
+#define SS_PIN 10     //SDA
+#define RST_PIN 14    //RST
+#define SCK_PIN 37    //SCK
+#define MOSI_PIN 39   //MOSI
+#define MISO_PIN 13   //MISO
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Create an instance of the MFRC522 class with specified SS and RST pins
-String tagID = "";                 // Declare a String variable to store the tag ID
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+String tagID = "";    //Stores TagID
+
+// Declare getTagID function before it's used
+String getTagID(byte *buffer, byte bufferSize); // Define function getTag id used to format string
+void readNFC();   //Define function readNFC used to read the nfc then calls function getTagID once tag is read
 
 void setup() {
-  Serial.begin(115200);         //Used for testing
-  
-  // Initialize SPI with custom pins
-  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, SS_PIN);  // Begin SPI communication
-  mfrc522.PCD_Init();             // Initialize the MFRC522 module
-
-  //Serial.println("Ready to read NFC cards!");  //Used for Testing
+  Serial.begin(115200);
+  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, SS_PIN);
+  mfrc522.PCD_Init();
+  Serial.println("Ready to read NFC cards!");
 }
 
-void readNFC() {
-  unsigned long startTime = millis();  // Record the start time
-  bool cardRead = false;  // Flag to indicate if a card has been read
+void loop() {
+  Serial.print("Start");
+  readNFC();
+  Serial.print("Card UID: ");
+  Serial.println(tagID);
+  Serial.print("END");
+  delay(2000);
+}
 
-  while (millis() - startTime < 10000) {  // Continue checking for cards for up to 10 seconds
-    // Look for new cards
-    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-      //Serial.print("Card UID: "); //Used for Testing
-      tagID = getTagID(mfrc522.uid.uidByte, mfrc522.uid.size);  // Get the tag ID and store it in the tagID variable
-      //Serial.println(tagID);  //Used for Testing, Print ID Card
-      cardRead = true;
-      break;  // Exit the loop if card is read
+bool readNFC() {
+  // Look for new cards
+  unsigned long startTime = millis();
+  while (millis() - startTime < 10000) {    //Loop this function for 10 seconds
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) { // Checking if NFCtag is present 
+      tagID = getTagID(mfrc522.uid.uidByte, mfrc522.uid.size); //Format the tag id
+      delay(1000);
+      return true; //Return true if tag is read
+      break;
+      
     }
+    return false; //Return false if no tag is read
   }
+}
 
-  // When card id read wait 1 second
-  if (cardRead) {
-    delay(1000);
+String getTagID(byte *buffer, byte bufferSize) {
+  String tag = "";
+  for (byte i = 0; i < bufferSize; i++) {
+    tag += (buffer[i] < 0x10 ? "0" : "") + String(buffer[i], HEX);
   }
-  
-  // If card is read return true if card is not read return false
-  return cardRead;
+  return tag;
 }
